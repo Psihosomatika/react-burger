61cb7152daf306f5782cb4f6202b9c149cd30d51
Diana Rukavishnikova <psihomatika@gmail.com>
2023-01-10 12:34:07 +0500

    add Header

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import appStyles from './app.module.css';
+// import mainImage from '../../images/main.jpg';
+
+import AppHeader from '../app-header/app-header';
+// import Main from '../main/main';
+
+class App extends React.Component {
+  render() {
+    // const mainData = {
+    //   title: "Группа поддержки",
+    //   description: "Это самые преданные болельщики, которые будут радоваться твоим успехам и грустить вместе с тобой в тяжёлые времена",
+    //   caption: "Нужно не забыть покормить их"
+    // }
+
+    return (
+      <div className={appStyles.app}>
+        <AppHeader />
+
+      </div>
+    );
+  }
+}
+
+export default App;
